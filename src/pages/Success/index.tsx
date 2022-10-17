dddcaef1dece4b373cde48057591d3c286bba245
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-10-17 19:57:13 -0300

    Finalização da página de success

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,82 @@
-import { SuccessContainer } from "./styles";
+import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
+import { useEffect } from "react";
+import illustration from "../../assets/confirmedOrder.svg"
+import { useLocation, useNavigate } from "react-router-dom";
+import { useTheme } from "styled-components";
+import { InfoWithIcon } from "../../components/InfoWithIcon";
+import { RegularText, TitleText } from "../../components/Typography";
+import { OrderData } from "../Checkout";
+import { paymentMethods } from "../Checkout/components/CompleteOrderForm/PaymentMethodOptions";
+import { OrderInfo, SuccessContainer } from "./styles";
+
+interface LocationType {
+    state: OrderData;
+}
 
 export function Success() {
+    const { colors } = useTheme();
+
+    const { state } = useLocation() as unknown as LocationType;
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!state) {
+            navigate("/");
+        }
+    }, []);
+
+    if (!state) return <></>;
+    
     return (
         <SuccessContainer>
-            
+            <div>
+                <TitleText size="l">Uhu! Pedido confirmado</TitleText>
+                <RegularText size="l">Agora é só aguardar que logo o café chegará até você</RegularText>
+            </div>
+
+            <section>
+                <OrderInfo>
+                    <InfoWithIcon 
+                        icon={<MapPin weight="fill" />}
+                        iconBg={colors.purple}
+                        text={
+                            <RegularText>
+                                Entrega em <strong>{state.street}</strong>, {state.number}
+                                <br />
+                                {state.district} - {state.city}, {state.uf}
+                            </RegularText>
+                        }
+                    />
+
+                    <InfoWithIcon
+                        icon={<Clock weight="fill" />}
+                        iconBg={colors["yellow"]}
+                        text={
+                        <RegularText>
+                            Previsão de entrega
+                            <br />
+                            <strong>20 min - 30 min</strong>
+                        </RegularText>
+                        }
+                    />
+
+                    <InfoWithIcon
+                        icon={<CurrencyDollar weight="fill" />}
+                        iconBg={colors["yellow-dark"]}
+                        text={
+                        <RegularText>
+                            Pagamento na entrega
+                            <br />
+                            <strong>{paymentMethods[state.paymentMethod].label}</strong>
+                        </RegularText>
+                        }
+                    />
+
+                </OrderInfo>
+                
+                <img src={illustration} />
+            </section>
         </SuccessContainer>
     )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,5 +1,39 @@
 import styled from "styled-components";
 
 export const SuccessContainer = styled.main`
-    
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem; 
+    margin-top: 5rem;
+
+    h1 {
+        color: ${({ theme }) => theme.colors["yellow-dark"]};
+    }
+
+    > section {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 `
+
+export const OrderInfo = styled.div`
+    margin-top: 2.5rem;
+    padding: 2.5rem;
+    border-radius: 6px 36px 6px 36px;
+    position: relative;
+    background: ${({ theme }) => theme.colors.background};
+    min-width: 32rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    
+    &::before {
+        content: "";
+        position: absolute;
+        inset: -1px;
+        z-index: -1;
+        border-radius: 7px 37px 7px 37px;
+        background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+    }
+`;
